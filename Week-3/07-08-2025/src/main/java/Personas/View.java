fca47fbaf1ad30b498d72ce2949406d475680a39
unknown <isaacjose04@gmail.com>
2025-08-07 09:32:24 -0600

    Final Cap

---
+++ Week-3/07-08-2025/src/main/java/Personas/View.java
@@ -0,0 +1,25 @@
+package Personas;
+
+import javax.swing.*;
+
+public class View {
+    private JTextField Id;
+    private JTextField name;
+    private JRadioButton maleRadioButton;
+    private JRadioButton femaleRadioButton;
+    private JRadioButton otherRadioButton;
+    private JPanel panel;
+    private JLabel maritalStatus;
+    private JComboBox comboBox1;
+    private JCheckBox musicCheckBox;
+    private JCheckBox videoGamesCheckBox;
+    private JCheckBox moviesCheckBox;
+    private JCheckBox cookCheckBox;
+    private JCheckBox sportsCheckBox;
+    private JCheckBox otherCheckBox;
+    private JTextField textField1;
+
+    public JPanel getPanel() {
+        return panel;
+    }
+}
